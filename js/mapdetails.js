17245a8f49e06742d87b6e7b990c778c9c3c5f4e
ottluuk <no.email>
2016-12-07 15:04:19 +0200

    omit secondary filters
    Former-commit-id: c1f44f8988a81f55819879007dc28bcb70b1a003

--- js/mapdetails.js
+++ js/mapdetails.js
@@ -44,51 +44,6 @@ function filterTable() {
   }
 }
 
-function filterTable3() {
-  // Declare variables 
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("table_filter_input_3");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("obs_table").getElementsByTagName("table")[0];
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[3];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
-
-function filterTablez() {
-  // Declare variables 
-  var input, filter, input3, filter3, table, tr, td, td3, i;
-  input = document.getElementById("table_filter_input");
-  filter = input.value.toUpperCase();
-  input3 = document.getElementById("table_filter_input_3");
-  filter3 = input3.value.toUpperCase();
-
-  table = document.getElementById("obs_table").getElementsByTagName("table")[0];
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    td3 = tr[i].getElementsByTagName("td")[3];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1 && td3.innerHTML.toUpperCase().indexOf(filter3) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
 
 
 
