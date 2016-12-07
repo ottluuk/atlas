5625f4e1711dff8a0d854679f128738f8607d432
ottluuk <no.email>
2016-12-07 14:36:49 +0200

    added separate filter for third col
    Former-commit-id: 3cd9fde9c4271f70d5ef73dfe54742eb526715e6

--- js/mapdetails.js
+++ js/mapdetails.js
@@ -44,3 +44,25 @@ function filterTable() {
   }
 }
 
+function filterTable3() {
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("table_filter_input_3");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("obs_table").getElementsByTagName("table")[0];
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
+
