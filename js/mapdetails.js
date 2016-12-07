ebe2d637d18afcaa04e19cbe1f326f0b01a27d4a
ottluuk <no.email>
2016-12-07 14:21:26 +0200

    add filterTable()
    Former-commit-id: 4caef2d1e6ce51fad52a4db4ac2fb76d3c3b5c37

--- js/mapdetails.js
+++ js/mapdetails.js
@@ -1,3 +1,4 @@
+// Primitive arrow navigation
 function leftArrowPressed() {
     var a;
     a = document.getElementById('prevLink')
@@ -19,3 +20,27 @@ document.onkeydown = function(evt) {
             break;
     }
 };
+
+// Table filter
+
+function filterTable() {
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("table_filter_input");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("obs_table").getElementsByTagName("table")[0];
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
