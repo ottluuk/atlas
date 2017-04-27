a7a9cc8606571a60dfd03431093110cd141e3a65
ottluuk <no.email>
2017-04-27 16:01:38 +0300

    fix species filter?
    Former-commit-id: c9f3ed1b654231b68a54ebaa4c5d18e59f4a4d38

--- js/quaddetails.js
+++ js/quaddetails.js
@@ -2,7 +2,7 @@
 
 function filterSpeciesTable() {
   // Declare variables 
-  var input, filter, table, tr, a, td, i;
+  var input, filter, table, tr, a, i;
   input = document.getElementById("species_table_filter_input");
   filter = input.value.toUpperCase();
   table = document.getElementById("species_table").getElementsByTagName("table")[0];
@@ -10,9 +10,8 @@ function filterSpeciesTable() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    a = td.getElementsByTagName("a")[0];
-    if (td) {
+    a = tr[i].getElementsByTagName("a")[0];
+    if (a) {
       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
